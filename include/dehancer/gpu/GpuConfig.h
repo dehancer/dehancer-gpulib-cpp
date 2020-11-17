06519374d3551bec24729f87ba9ba273d195a42d
Денис Свинарчук <denn.nevera@gmail.com>
2020-11-17 14:24:40 +0300

    both platforms can be build simultaneously

--- include/dehancer/gpu/GpuConfig.h
+++ include/dehancer/gpu/GpuConfig.h
@@ -3,13 +3,3 @@
 //
 
 #include "dehancer/gpu/GpuTypedefs.h"
-
-#if defined(DEHANCER_GPU_METAL)
-
-#elif defined(DEHANCER_GPU_OPENCL)
-
-#else
-
-#error "You must define GPU Layer"
-
-#endif
---
+++ lib/opencl/cmake/dehancer_gpulib_opencl.cmake
@@ -0,0 +1,8 @@
+add_library(${PROJECT_LIB} STATIC IMPORTED)
+
+find_library(${PROJECT_LIB}_LIBRARY_PATH ${PROJECT_LIB} HINTS "../../..")
+set_target_properties(${PROJECT_LIB} PROPERTIES IMPORTED_LOCATION "${dehancer_gpulib_opencl_LIBRARY_PATH}")
+
+include_directories(
+        "${dehancer_gpulib_opencl_INCLUDE_PATH}"
+)
