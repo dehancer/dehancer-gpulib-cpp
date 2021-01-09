ca6d69656f6e5a52c3e2e9d9bdad072a5c8c487e
denn (ubuntu20.04) <denn.nevera@gmail.com>
2021-01-09 16:47:55 +0300

    gaussian blur added

--- include/dehancer/gpu/operations/GaussianBlur.h
+++ include/dehancer/gpu/operations/GaussianBlur.h
@@ -4,32 +4,32 @@
 
 #pragma once
 
-#include "dehancer/gpu/Kernel.h"
-#include "dehancer/gpu/Channels.h"
+//#include "dehancer/gpu/Kernel.h"
+#include "dehancer/gpu/operations/UnaryKernel.h"
 
 namespace dehancer {
 
-    class GaussianBlur: public ChannelsInput {
+    class GaussianBlur: public UnaryKernel {
     public:
 
         GaussianBlur(const void* command_queue,
-                     const Texture& s,
-                     const Texture& d,
+                     const Texture&    s,
+                     const Texture&    d,
                      std::array<int,4> radius,
+                     EdgeAddress       address_mode = EdgeAddress::ADDRESS_CLAMP,
+                     float             accuracy = 0.001,
                      bool wait_until_completed = WAIT_UNTIL_COMPLETED,
                      const std::string& library_path = ""
         );
-
-        void process() override;
-
-    private:
-        int box_number_ = 3;
-        std::array<int,4> radius_;
-        std::array<std::vector<float>,4> radius_boxes_;
-        size_t w_;
-        size_t h_;
-        Channels channels_out_;
-        ChannelsOutput channels_finalizer_;
+        
+//    private:
+//        int box_number_ = 3;
+//        std::array<int,4> radius_;
+//        std::array<std::vector<float>,4> radius_boxes_;
+//        size_t w_;
+//        size_t h_;
+//        Channels channels_out_;
+//        ChannelsOutput channels_finalizer_;
     };
 }
 
--- src/operations/GaussianBlur.cpp
+++ src/operations/GaussianBlur.cpp
@@ -3,131 +3,181 @@
 //
 
 #include "dehancer/gpu/operations/GaussianBlur.h"
+
+#include <cmath>
 #include "dehancer/gpu/math/GaussianUtils.h"
 
 namespace dehancer {
-
-    namespace _blur_impl_ {
-        class SwapChannels: public Function {
-        public:
-            SwapChannels(const void *command_queue,
-                         const Memory& channel_in,
-                         const Memory& channel_out,
-                         size_t w,
-                         size_t h,
-                         bool wait_until_completed = WAIT_UNTIL_COMPLETED,
-                         const std::string& library_path = ""):
-                    Function(command_queue, "swap_channels_kernel", wait_until_completed, library_path),
-                    channel_in_(channel_in),
-                    channel_out_(channel_out),
-                    w_(w),
-                    h_(h){}
-
-            void process() {
-              execute([this](CommandEncoder &command) {
-                  command.set(channel_in_, 0);
-                  command.set(channel_out_, 1);
-                  int w = w_, h = h_;
-                  command.set(&w, sizeof(w), 2);
-                  command.set(&h, sizeof(h), 3);
-                  return (CommandEncoder::Size) {this->w_, this->h_, 1};
-              });
-            }
-
-        protected:
-            const Memory& channel_in_;
-            const Memory& channel_out_;
-            size_t w_;
-            size_t h_;
-        };
-
-        class BoxBlurChannels: public SwapChannels {
-        public:
-            BoxBlurChannels(const void *command_queue,
-                            const Memory& channel_in,
-                            const Memory& channel_out,
-                            size_t w,
-                            size_t h,
-                            int radius,
-                            bool wait_until_completed = WAIT_UNTIL_COMPLETED,
-                            const std::string& library_path = ""):
-                    SwapChannels(command_queue, channel_in, channel_out, w, h , wait_until_completed, library_path),
-                    radius_(radius),
-                    box_blur_horizontal_kernel_(new Function(command_queue, "box_blur_horizontal_kernel", "")),
-                    box_blur_vertical_kernel_(new Function(command_queue, "box_blur_vertical_kernel", ""))
-            {
-
-            }
-
-            void process() {
-
-              SwapChannels::process();
-
-              box_blur_horizontal_kernel_->execute([this](CommandEncoder& command){
-                  command.set(this->channel_out_,0);
-                  command.set(this->channel_in_,1);
-                  int w = this->w_, h = this->h_;
-                  command.set(&w,sizeof(w),2);
-                  command.set(&h,sizeof(h),3);
-                  command.set(&this->radius_,sizeof(this->radius_),4);
-                  return (CommandEncoder::Size){this->w_,this->h_,1};
-              });
-
-              box_blur_vertical_kernel_->execute([this](CommandEncoder& command){
-                  command.set(this->channel_in_,0);
-                  command.set(this->channel_out_,1);
-                  int w = this->w_, h = this->h_;
-                  command.set(&w,sizeof(w),2);
-                  command.set(&h,sizeof(h),3);
-                  command.set(&this->radius_,sizeof(this->radius_),4);
-                  return (CommandEncoder::Size){this->w_,this->h_,1};
-              });
-            }
-
-        private:
-            int radius_;
-            std::shared_ptr<Function> box_blur_horizontal_kernel_;
-            std::shared_ptr<Function> box_blur_vertical_kernel_;
-        };
-    }
-
-    GaussianBlur::GaussianBlur(const void* command_queue,
-                               const Texture& s,
-                               const Texture& d,
-                               std::array<int,4> radius,
-                               bool wait_until_completed,
-                               const std::string& library_path
-    ):
-            ChannelsInput (command_queue, s, wait_until_completed, library_path),
-            radius_(radius),
-            w_(s->get_width()),
-            h_(s->get_height()),
-            channels_out_(ChannelsHolder::Make(command_queue,s->get_width(),s->get_height())),
-            channels_finalizer_(command_queue, d, channels_out_, wait_until_completed)
+    
+    struct GaussianBlurOptions {
+        std::array<int, 4> radius_array;
+        float  accuracy;
+    };
+    
+    auto kernel_blur = [](int index, std::vector<float>& data, const std::optional<std::any>& user_data) {
+        
+        data.clear();
+        
+        auto options = std::any_cast<GaussianBlurOptions>(user_data.value());
+        
+        auto radius = options.radius_array.at(index);
+        
+        if (radius==0) return ;
+        
+        float sigma = radius/2.0f;
+        int kRadius = (int)std::ceil(sigma*std::sqrt(-2.0f*std::log(options.accuracy)))+1;
+        
+        auto size = kRadius;
+        if (size%2==0) size+=1;
+        if (size<3) size=3;
+        
+        dehancer::math::make_gaussian_kernel(data, size, radius/2.0f);
+    };
+    
+    GaussianBlur::GaussianBlur (const void *command_queue,
+                                const Texture &s,
+                                const Texture &d,
+                                std::array<int, 4> radius,
+                                EdgeAddress    address_mode,
+                                float             accuracy,
+                                bool wait_until_completed,
+                                const std::string &library_path):
+            UnaryKernel(command_queue,s,d,{
+                                .row = kernel_blur,
+                                .col = kernel_blur,
+                                .user_data = (GaussianBlurOptions){radius,accuracy},
+                                .address_mode = address_mode
+                        },
+                        wait_until_completed,
+                        library_path)
     {
-      for (int i = 0; i < radius_.size(); ++i) {
-        dehancer::math::make_gaussian_boxes(radius_boxes_[i], radius_[i]/2, box_number_);
-      }
     }
+    
+    //
+    // experimental blur as boxed blur
+    //
 
-    void GaussianBlur::process() {
+//    namespace _blur_impl_ {
+//        class SwapChannels: public Function {
+//        public:
+//            SwapChannels(const void *command_queue,
+//                         const Memory& channel_in,
+//                         const Memory& channel_out,
+//                         size_t w,
+//                         size_t h,
+//                         bool wait_until_completed = WAIT_UNTIL_COMPLETED,
+//                         const std::string& library_path = ""):
+//                    Function(command_queue, "swap_channels_kernel", wait_until_completed, library_path),
+//                    channel_in_(channel_in),
+//                    channel_out_(channel_out),
+//                    w_(w),
+//                    h_(h){}
+//
+//            void process() {
+//              execute([this](CommandEncoder &command) {
+//                  command.set(channel_in_, 0);
+//                  command.set(channel_out_, 1);
+//                  int w = w_, h = h_;
+//                  command.set(&w, sizeof(w), 2);
+//                  command.set(&h, sizeof(h), 3);
+//                  return (CommandEncoder::Size) {this->w_, this->h_, 1};
+//              });
+//            }
+//
+//        protected:
+//            const Memory& channel_in_;
+//            const Memory& channel_out_;
+//            size_t w_;
+//            size_t h_;
+//        };
+//
+//        class BoxBlurChannels: public SwapChannels {
+//        public:
+//            BoxBlurChannels(const void *command_queue,
+//                            const Memory& channel_in,
+//                            const Memory& channel_out,
+//                            size_t w,
+//                            size_t h,
+//                            int radius,
+//                            bool wait_until_completed = WAIT_UNTIL_COMPLETED,
+//                            const std::string& library_path = ""):
+//                    SwapChannels(command_queue, channel_in, channel_out, w, h , wait_until_completed, library_path),
+//                    radius_(radius),
+//                    box_blur_horizontal_kernel_(new Function(command_queue, "box_blur_horizontal_kernel", "")),
+//                    box_blur_vertical_kernel_(new Function(command_queue, "box_blur_vertical_kernel", ""))
+//            {
+//
+//            }
+//
+//            void process() {
+//
+//              SwapChannels::process();
+//
+//              box_blur_horizontal_kernel_->execute([this](CommandEncoder& command){
+//                  command.set(this->channel_out_,0);
+//                  command.set(this->channel_in_,1);
+//                  int w = this->w_, h = this->h_;
+//                  command.set(&w,sizeof(w),2);
+//                  command.set(&h,sizeof(h),3);
+//                  command.set(&this->radius_,sizeof(this->radius_),4);
+//                  return (CommandEncoder::Size){this->w_,this->h_,1};
+//              });
+//
+//              box_blur_vertical_kernel_->execute([this](CommandEncoder& command){
+//                  command.set(this->channel_in_,0);
+//                  command.set(this->channel_out_,1);
+//                  int w = this->w_, h = this->h_;
+//                  command.set(&w,sizeof(w),2);
+//                  command.set(&h,sizeof(h),3);
+//                  command.set(&this->radius_,sizeof(this->radius_),4);
+//                  return (CommandEncoder::Size){this->w_,this->h_,1};
+//              });
+//            }
+//
+//        private:
+//            int radius_;
+//            std::shared_ptr<Function> box_blur_horizontal_kernel_;
+//            std::shared_ptr<Function> box_blur_vertical_kernel_;
+//        };
+//    }
 
-      ChannelsInput::process();
-
-      for (int i = 0; i < channels_out_->size(); ++i) {
-        auto in = this->get_channels()->at(i);
-        auto out = channels_out_->at(i);
-        for (int j = 0; j < box_number_; ++j) {
-          auto r = (radius_boxes_[i][j]-1)/2;
-          if (r==0){
-            _blur_impl_::SwapChannels(get_command_queue(), in, out, w_, h_, r).process();
-            break;
-          }
-          _blur_impl_::BoxBlurChannels(get_command_queue(), in, out, w_, h_, r).process();
-          std::swap(in,out);
-        }
-      }
-
-      channels_finalizer_.process();
-    }
+//    GaussianBlur::GaussianBlur(const void* command_queue,
+//                               const Texture& s,
+//                               const Texture& d,
+//                               std::array<int,4> radius,
+//                               bool wait_until_completed,
+//                               const std::string& library_path
+//    ):
+//            ChannelsInput (command_queue, s, wait_until_completed, library_path),
+//            radius_(radius),
+//            w_(s->get_width()),
+//            h_(s->get_height()),
+//            channels_out_(ChannelsHolder::Make(command_queue,s->get_width(),s->get_height())),
+//            channels_finalizer_(command_queue, d, channels_out_, wait_until_completed)
+//    {
+//      for (int i = 0; i < radius_.size(); ++i) {
+//        dehancer::math::make_gaussian_boxes(radius_boxes_[i], radius_[i]/2, box_number_);
+//      }
+//    }
+//
+//    void GaussianBlur::process() {
+//
+//      ChannelsInput::process();
+//
+//      for (int i = 0; i < channels_out_->size(); ++i) {
+//        auto in = this->get_channels()->at(i);
+//        auto out = channels_out_->at(i);
+//        for (int j = 0; j < box_number_; ++j) {
+//          auto r = (radius_boxes_[i][j]-1)/2;
+//          if (r==0){
+//            _blur_impl_::SwapChannels(get_command_queue(), in, out, w_, h_, r).process();
+//            break;
+//          }
+//          _blur_impl_::BoxBlurChannels(get_command_queue(), in, out, w_, h_, r).process();
+//          std::swap(in,out);
+//        }
+//      }
+//
+//      channels_finalizer_.process();
+//    }
 }
