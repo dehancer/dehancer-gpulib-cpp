76de006f920077b4a323ff74b1b2c9043d26ba90
denn nevera (G5) <denn.nevera@gmail.com>
2021-04-19 18:45:16 +0300

    clang 10 supported tests fixes

--- tests/operations/gamma/cuda/main_cuda.cpp
+++ tests/operations/gamma/cuda/main_cuda.cpp
@@ -23,7 +23,7 @@ namespace dehancer::device {
       * @return metal lib path.
       */
     std::string get_lib_path() {
-      return METAL_KERNELS_LIBRARY;
+      return CUDA_KERNELS_LIBRARY;
     }
 
     extern std::size_t get_lib_source(std::string& source) {
