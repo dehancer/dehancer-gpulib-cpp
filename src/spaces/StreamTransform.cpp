cb74780c688909e9f34c4ff162535dd894cc7838
denn nevera (Air M1) <denn.nevera@gmail.com>
2021-08-13 16:21:51 +0300

    step 5

--- src/spaces/StreamTransform.cpp
+++ src/spaces/StreamTransform.cpp
@@ -46,7 +46,7 @@ namespace dehancer {
      //// if (transform_lut)
         encoder.set(transform_lut->get_texture(), 2);
       
-      //encoder.set(space_,3);
+      encoder.set(space_,3);
       encoder.set(direction_,4);
       encoder.set(transform_lut_enabled,5);
       encoder.set(transform_function_enabled,6);
