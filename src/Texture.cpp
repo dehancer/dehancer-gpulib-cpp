d801fc43c217fedc419996617955f16b8726026e
denn nevera m1max 14 <denn.nevera@gmail.com>
2023-04-16 00:57:18 +0300

    code cleaned up

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -108,8 +108,7 @@ namespace dehancer {
     }
     
     Texture TextureHolder::Flip (const Texture &texture, FlipMode mode) {
-      //return std::move(Flip(texture, mode, texture->get_desc().pixel_format));
-      return Flip(texture, mode, texture->get_desc().pixel_format);
+      return std::move(Flip(texture, mode, texture->get_desc().pixel_format));
     }
     
     Texture TextureHolder::Flip (const Texture &texture, FlipMode mode, TextureDesc::PixelFormat format) {
@@ -136,8 +135,7 @@ namespace dehancer {
           return dehancer::CommandEncoder::Size::From(result);
       });
       
-      //return std::move(result);
-      return result;
+      return std::move(result);
     }
     
     Texture TextureHolder::Rotate90 (const Texture &texture, Rotate90Mode mode) {
