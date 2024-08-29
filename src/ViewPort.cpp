62a6cf35d5e2c9a4a8b70cb02a3f726a03636cc5
Dmitry Sukhorukov <funnycodes@gmail.com>
2024-08-29 15:01:22 +0300

    Fix halation/bloom for viewport

--- src/ViewPort.cpp
+++ src/ViewPort.cpp
@@ -9,8 +9,11 @@
 #include <algorithm>
 
 namespace dehancer {
-    ViewPortHolder::ViewPortHolder(const viewport::Origin &origin, const viewport::Size &size, const viewport::Size &source_size)
-    : _origin(origin), _size(size), _source_size(source_size) {
+ViewPortHolder::ViewPortHolder(const viewport::Origin &origin
+                               , const viewport::Size &size
+                               , const viewport::Size &source_size
+                               , const viewport::Size &original_size)
+    : _origin(origin), _size(size), _source_size(source_size), _original_size(original_size){
         _origin.x = std::clamp(_origin.x, 0.f, 1.f);
         _origin.y = std::clamp(_origin.y, 0.f, 1.f);
 
@@ -18,9 +21,12 @@ namespace dehancer {
         if(_origin.y + _size.height > 1.f) _size.height = 1.f - _origin.y;
     }
 
-    ViewPort ViewPortHolder::Make(const viewport::Origin &origin, const viewport::Size &size, const viewport::Size &source_size)
+ViewPort ViewPortHolder::Make(const viewport::Origin &origin
+                              , const viewport::Size &size
+                              , const viewport::Size &source_size
+                              , const viewport::Size &original_size)
     {
-        return std::make_shared<ViewPortHolder>(origin, size, source_size);
+        return std::make_shared<ViewPortHolder>(origin, size, source_size, original_size);
     }
 
     const viewport::Origin& ViewPortHolder::get_origin() const {
@@ -33,5 +39,9 @@ namespace dehancer {
 
     const viewport::Size& ViewPortHolder::get_source_size() const {
         return _source_size;
+    }
+
+    const viewport::Size& ViewPortHolder::get_original_size() const {
+        return _original_size;
     }
 }
