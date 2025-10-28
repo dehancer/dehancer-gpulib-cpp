a7b01b0f7706ddf5c32e8b69539467694aa7204e
denis svinarchuk <denn.nevera@gmail.com>
2025-10-28 07:06:07 +0000

    fixes

--- include/dehancer/gpu/kernels/clut_kernels.h
+++ include/dehancer/gpu/kernels/clut_kernels.h
@@ -7,7 +7,7 @@
 
 #include "dehancer/gpu/kernels/common.h"
 #include "dehancer/gpu/kernels/types.h"
-#include "dehancer/devices/transform_lut1d.h"
+#include "dehancer/devices/lut1d_sampler.h"
 
 //
 // MARK - Identity
